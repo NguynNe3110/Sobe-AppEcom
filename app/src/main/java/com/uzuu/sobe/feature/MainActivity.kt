29a5358203959491407cbcb86da4ddbe760ca3cc
NguyenUzuu <nguyen12112005@gmail.com>
2026-06-05 05:26:20 +0700

    add enableEdgeToEdge() in mainact

--- app/src/main/java/com/uzuu/sobe/feature/MainActivity.kt
+++ app/src/main/java/com/uzuu/sobe/feature/MainActivity.kt
@@ -3,6 +3,7 @@ package com.uzuu.sobe.feature
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Shapes
@@ -17,6 +18,7 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity: ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
 
         setContent {
             MaterialTheme(
